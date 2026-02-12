6b0f00e0bd15109f9885b5610a01718bd1c5bd89
kavita-dubey02 <dubeykavita870@gmail.com>
2026-02-12 16:31:06 +0530

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
          $this->call(RoleSeeder::class);
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
         
     }
    
