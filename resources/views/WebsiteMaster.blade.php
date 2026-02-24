4bd9a47e6d2d4d27e517f0e1e12fd2c9715e6cae
kavita-dubey02 <dubeykavita870@gmail.com>
2026-02-24 13:51:03 +0530

    removebtn

--- resources/views/WebsiteMaster.blade.php
+++ resources/views/WebsiteMaster.blade.php
@@ -610,8 +610,7 @@
     <a href="{{ route('web-admin.logout') }}" class="header-contact-btn d-none d-lg-block">
     <i class="fa-solid fa-right-to-bracket me-2"></i> logout
 </a>
- <a href="{{ route('web-admin.dashboard') }}" class="header-contact-btn d-none d-lg-block">
-    <i class="fa-solid fa-bars me-2"></i>
+
     @else
   <!-- Contact Button -->
       <a href="{{ route('login') }}" class="header-contact-btn d-none d-lg-block">
